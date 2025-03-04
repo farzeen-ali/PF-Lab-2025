cba94c5a960a06cdeb3343d4337d76065ced37ec
farzeen-ali <farzeenaliarif786@gmail.com>
2025-03-04 13:10:11 +0500

    Variables & Data Types

---
+++ C Code.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	int myAge; //Variable Declaration
+	printf("Enter Your Age: ");
+	scanf("%d", &myAge);
+	printf("Your age is: %d", myAge);
+	return 0;
+}
+
---
+++ Variables & Data Types.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Integer types
+    int num = 10;
+    short smallNum = 5;
+    long bigNum = 100000L;
+    long long veryBigNum = 10000000000LL;
+    unsigned int positiveNum = 50;
+
+    // Floating-point types
+    float pi = 3.14f;
+    double precisePi = 3.1415926535;
+    long double veryPrecisePi = 3.141592653589793238L;
+
+    // Character types
+    char letter = 'A';
+
+    // Boolean 
+    bool isFalse = false;
+
+    // String in C (Character Array)
+    char str1[] = "Hello, World!";  // String using character array
+
+    return 0;
+}
+

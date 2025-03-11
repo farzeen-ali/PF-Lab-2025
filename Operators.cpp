411dcf744e99380361559cedc5212b0e27eb7969
farzeen-ali <farzeenaliarif786@gmail.com>
2025-03-11 12:12:01 +0500

    Operators

--- Operators.cpp
+++ Operators.cpp
@@ -1,67 +1,76 @@
-//Operators --> +,-,*,/,....
-//Operands ---> 1,2, a,b 
-//Expression ---> 2+3, a+b
+// Operands
+// Operators
+// Expression
 
 #include<stdio.h>
+
 int main(){
-	// 3
-	// Unary, Binary, Ternary 
+	// Unary, Binary, Ternary
 	
-	// Unary Arithmetic Operators
-	// Increment, Decrement
-//	int num1; //Declaration
-//	num1 = 3; //Initialization	
+	//Unary
+	// Increment, Decremenet
+	// Post, Pre
+	//int a = 2;
+//	a++;
+	//printf("%d\n",a++); // Post Increment
+	//printf("%d\n",a);
+	//printf("%d\n", ++a); // Pre Increment
+	//printf("%d\n",a);
+	//printf("%d\n",a--); // Post Decrement
+	//printf("%d\n",a);
+	//printf("%d\n", --a); // Post Decrement
 	
-	// Pre Increment, ++
-//	printf("%d",++num1);
-	// Post Increment
-//	printf("%d",num1++);
+	// Binary 
+	// Arithmetic +, -, *,/, %
+	int num1, num2;
+	num1 = 2;
+	num2 = 4;
+	//printf("%d\n",(num1+num2));
+	//printf("%d\n",(num1-num2));
+	//printf("%d\n",(num1*num2));
+	//printf("%d\n",(num1/num2));
+	//printf("%d\n",(num1%num2));
 	
-	// Pre Decrement, ++
-//	printf("%d",--num1);
-	// Post Decrement
-//	printf("%d",num1--);
-	
-	// Binary Arithmetic Operators
-	// +, -, *, /, %
 //	int value1, value2;
-//	printf("Enter Two Numbers: ");
-//	scanf("%d", &value1);
-//	scanf("%d", &value2);
-//	printf("Sum: %d", value1+value2);
-//	printf("Sub: %d", value1-value2);
-//	printf("Mul: %d", value1*value2);
-//	printf("Div: %d", value1/value2);
-//	printf("Remainder: %d", value1%value2);
+//	value1 = 4;
+//	value2 = 3;
+//	printf("%d\n", (value1%value2));
 	
 	// Relational Operators
-//	int x = 5, y = 10;
-//	printf("%d\n", (x<y));
-//	printf("%d\n", (x>y));
-//	printf("%d\n", (x<=y));
-//	printf("%d\n", (x>=y));
-//	printf("%d\n", (x==y));
-//	printf("%d\n", (x!=y));
 	
-	// Logical Operators
-	// &&, ||, !
-//	int a = 0, b = 0;
-//	printf("%d\n", (a && b));
-//	printf("%d\n", (a || b));
-//	printf("%d\n", (!a));
+//	printf("%d\n", (num1<num2)); //1
+//	printf("%d\n", (num1<=num2)); //1
+//	printf("%d\n", (num1>num2)); //0
+//	printf("%d\n", (num1>=num2)); //0
+//	printf("%d\n", (num1==num2)); //0
+//	printf("%d\n", (num1!=num2)); //1
 	
-	// Assignment Operators
-	// =, +=, -=, *=, /=, %=
-	//int c = 2;
-	//printf("%d\n", (c+=3)); // c = c+3;
-	//printf("%d\n", (c-=5)); // c = c-5;
-	//printf("%d\n", (c*=3)); // c = c*3;
-	//printf("%d\n", (c/=3)); // c = c/3;
-	//printf("%d\n", (c%=2)); // c = c%2;
+	// Logical Operators: &&, ||, !
+//	int x = 0, y =1;
+//	printf("%d\n", (x && y)); //0
+//	printf("%d\n", (x || y)); //1
+//	printf("%d\n", (!x)); //1
+
+	//Assignment Operators
+	// =,+=, -=, *=, /=, %=
+//	a+=2; a=a+2;
+	//int  a= 2, b =3;
+//	printf("%d\n", a+=b); // a = a+b; // 5
+//	printf("%d\n", a-=b); // a = 5 - 3 //2
+//	printf("%d\n", a*=b); // a = 2 * 3 // 6
+//	printf("%d\n", a/=b); // a = 6 / 3 //2
+	//printf("%d\n", a%=b); // a = 2%3
 	
-	// Ternary Operator
-//	int num = 6;
+	// Ternary Operators
+//	int num = 7;
 //	printf("Your Result is: %s\n",
-//	(num % 2 == 0)?"Even":"Odd");
+//	(num % 2 == 0) ? "Odd" : "Even" );
+	
 	return 0;
 }
+
+
+
+
+
+

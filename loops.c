247813e385072fc721b2e085737b1bb2020b2f78
farzeen-ali <farzeenaliarif786@gmail.com>
2025-03-24 12:57:00 +0500

    Loops

---
+++ loops.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    // Initialization, condition, updation
+    // for loop
+    // for(int i = 0; i < 10; i++){
+    //     printf("%d Hello, World!",i);
+    // }
+    // for(int i = 2; i <= 20; i+=2){// i+=2; i = i+2;
+    //     printf("Your Number is: %d\n",i);
+    // } 
+
+    // while loop
+    // int a = 1;
+    // while(a<10){
+    //     printf("%d\n",a);
+    //     a++;
+    // }
+
+    // int num;
+    // printf("Enter Any Positive Number: ");
+    // scanf("%d", &num);
+
+    // while(num < 0){
+    //     printf("Enter Any Positive Number: ");
+    //     scanf("%d", &num);
+    // }
+    // printf("Your +ve Number is: %d", num);
+
+    // do-while loop
+
+//    int i = 8;
+//
+//    do{
+//        printf("%d\n",i);
+//        i++;
+//    } 
+//    while(i<5);
+
+//	char password[20]; // string
+//	char confirmPassword[20];
+//	
+//	do{
+//		printf("Enter Password: ");
+//		scanf("%s", password);
+//		printf("Enter Confirm Password: ");
+//		scanf("%s", confirmPassword);
+//		
+//		if(strcmp(password, confirmPassword) != 0){
+//			printf("Try Again!");
+//		}
+//		
+//	} while(strcmp(password, confirmPassword) != 0);
+//	printf("Login");
+
+	
+	//	for(int j = 0; j < 10; j++){
+//		if(j == 5){
+//			break;
+//		}		
+//		printf("%d",j);
+//	}
+
+//	for(int i = 0; i < 10; i++){
+//		if(i == 5){
+//			continue; //skip
+//		}	
+//		printf("%d",i);	
+//	}
+
+//	printf("One");
+//	goto a;
+//	b:
+//	printf("Two");
+//	a:
+//	printf("Three");
+//	goto b;
+
+	
+    return 0;
+}
